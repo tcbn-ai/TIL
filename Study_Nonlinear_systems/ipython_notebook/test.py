77e196208d75da1bc9842ced761cdb6fd993ee03
Kosuke Toda <td49ksk@gmail.com>
2021-08-29 14:14:43 +0900

    分岐現象 プログラム 修正

--- Study_Nonlinear_systems/ipython_notebook/test.py
+++ Study_Nonlinear_systems/ipython_notebook/test.py
@@ -1,9 +1,14 @@
 import numpy as np 
-init_lst = np.linspace(-5, 5, 11)
+init_lst = np.linspace(-3, 3, 7)
 initial_value_list = []
 for i in range(len(init_lst)):
     for j in range(len(init_lst)):
         initial_value = [round(init_lst[i], 2), round(init_lst[j], 2)]
+        initial_value_list.append(initial_value)
+init_lst_2 = np.linspace(-0.5, 0.5, 11)
+for i in range(len(init_lst_2)):
+    for j in range(len(init_lst_2)):
+        initial_value = [round(init_lst_2[i], 2), round(init_lst_2[j], 2)]
         initial_value_list.append(initial_value)
 print(initial_value_list)
 
